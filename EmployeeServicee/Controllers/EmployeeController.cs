438abd427082f4ee3078a273f6a389261cf1fb77
Baddam Akhila <Baddam Akhila@LAPTOP-0C57MPBI>
2021-08-30 09:54:19 +0530

    Add project files.

---
+++ EmployeeServicee/Controllers/EmployeeController.cs
@@ -0,0 +1,79 @@
+﻿using EmployeeServicee.Data;
+using EmployeeServicee.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EmployeeServicee.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        EmployeeContext _context;
+        public EmployeeController(EmployeeContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public IActionResult CreateEmployee(Employee emp)
+        {
+            _context.Employee.Add(emp);
+            _context.SaveChanges();
+            return Ok();
+        }
+        [HttpGet]
+        public List<Employee> GetEmployee()
+        {
+            var emp = _context.Employee.ToList();
+            return emp;
+        }
+        [HttpGet]
+        public IActionResult test()
+        {
+            return Ok();
+        }
+        [HttpGet]
+        public Employee GetEmployeeById(int id)
+        {
+            var emp = _context.Employee.FirstOrDefault(e=>e.Id==id);
+            return emp;
+        }
+        [HttpPost]
+        public IActionResult DeleteEmp(int Id)
+        {
+            var result = _context.Employee.FirstOrDefault(e => e.Id == Id);
+            if (result != null)
+            {
+                _context.Employee.Remove(result);
+                _context.SaveChanges();
+                return Ok();
+            }
+            return NotFound();
+        }
+        [HttpGet]
+        public string Getusername(string Email)
+        {
+            var name = Email.Split('@')[0];
+            return name;
+        }
+        [HttpPost]
+        public IActionResult employeeUpdate(Employee emp)
+        { 
+            var result = _context.Employee.FirstOrDefault(e => e.Id == emp.Id);
+                if (result != null)
+                {
+                    result.Age = emp.Age;
+                    result.Name = emp.Name;
+                    result.Country = emp.Country;
+                    result.Designation = emp.Designation;
+                    result.ContactNumber = emp.ContactNumber;
+                    _context.SaveChanges();
+                    return Ok();
+                }
+                return NotFound();
+            }
+        }
+    }
+
---
+++ EmployeeServicee/Controllers/TestController.cs
@@ -0,0 +1,78 @@
+﻿using EmployeeServicee.Data;
+using EmployeeServicee.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeServicee.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        EmployeeContext _context;
+        public TestController(EmployeeContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public string Stringreverse(string s)
+
+        {
+            string result = string.Join(' ', s
+                .Split(' ').
+                Select(x => new string(x.Reverse().ToArray())));
+            return result;
+        }
+        [HttpGet]
+        public string Namingsplit(string name)
+        {
+            string first = name.Split(' ')[0];
+            return first;
+        }
+        [HttpGet]
+        public string convertnameUpper(string txt)
+        {
+            string txt1 = txt.ToUpper();
+            return txt1;
+        }
+        [HttpGet]
+        public string convertnameLower(string txt)
+        {
+            string txt1 = txt.ToLower();
+            return txt1;
+        }
+        [HttpGet]
+        public bool Comparing(string txt, string txt1)
+        {
+            if (string.Compare(txt, txt1) == 0)
+            {
+                return true;
+            }
+
+            else { }
+            return false;
+        }
+        [HttpGet]
+        public int leng(string str)
+        {
+            int str1 = str.Length;
+            return str1;
+        }
+        [HttpGet]
+        public string concat(string str1,string str2)
+        {
+            string name = string.Concat(str1, str2);
+            return name;
+        }
+        [HttpGet]
+        public int index(string str)
+        {
+            int name = str.IndexOf("i");
+            return name;
+        }
+    }
+}
